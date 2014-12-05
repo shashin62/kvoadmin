05a4f18a26413fb542ceb57f5515266e2c69ff63
sanily2j <shet.sanil@gmail.com>
2014-12-05 16:18:19 +0530

    UserController

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -170,6 +170,7 @@ Class UserController extends AppController {
                 $peopleData['People']['first_name'] = $this->request->data['User']['first_name'];
                 $peopleData['People']['last_name'] = $this->request->data['User']['last_name'];
                 $peopleData['People']['email'] = $this->request->data['User']['email'];
+                $peopleData['People']['phone_number'] = $this->request->data['User']['phone_number'];
                 $peopleData['People']['gender'] = $this->request->data['User']['gender'];
                 $peopleData['People']['martial_status'] = $this->request->data['User']['martial_status'];
                 $peopleData['People']['date_of_birth'] = $this->request->data['User']['date_of_birth'];
