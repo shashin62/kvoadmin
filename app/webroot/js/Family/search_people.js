ee2b33643dc26484d76cd74ff441f7c34fd70847
Sanil Shet <shet.sanil@gmail.com>
2014-12-06 10:43:30 +0530

    app helper functions

--- app/webroot/js/Family/search_people.js
+++ app/webroot/js/Family/search_people.js
@@ -95,7 +95,7 @@ function insertUser(id, data)
             showJsSuccessMessage(displayMsg);
             setTimeout(function () {
                 $('.jssuccessMessage').hide('slow');
-                window.location = baseUrl + 'family/familiyGroups';
+                window.location = baseUrl + '/family/familiyGroups';
             }, 2500);
         }
     });
