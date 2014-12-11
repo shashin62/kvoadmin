89436111d4af22d4b8fa672a814a09901b3a9c06
sanily2j <shet.sanil@gmail.com>
2014-12-11 09:14:37 +0530

    Country Masters - 911

--- app/Controller/CountryController.php
+++ app/Controller/CountryController.php
@@ -30,6 +30,53 @@ Class CountryController extends AppController {
     
     public function delete()
     {
+         $this->autoRender = false;
+        $id = $_REQUEST['id'];    
+        
+        if ($this->Country->delete(array('id' =>$id)) ) {
+            $msg['success'] = 1;
+            $msg['message'] = 'Education has been deleted';
+        } else {
+            $msg['success'] = 0;
+            $msg['message'] = 'System Error, Please try again';
+        }
+        
+        $this->set(compact('msg'));
+        $this->render("/Elements/json_messages");
+    }
+    
+    public function add()
+    {
+       $this->layout = 'ajax';
+        $this->autoRender = false;
+       
+        $this->request->data['Country']['created'] = date('Y-m-d H:i:s');
+        $this->request->data['Country']['status'] = 1;
+        
+        $result = $this->Country->checkCountryExists($this->request->data['Country']['name']);
+        $msg['status'] = 1;
+        if (!empty($result) && $this->request->data['Country']['id'] == '') {
+            $msg['status'] = 0;
+            $msg['error']['name'][] = "name";
+            $msg['error']['errormsg'][] = __('This name already exists.');
+        }
+        
+        $data = $this->request->data;
+         if ($msg['status'] == 1) {
+            if ($this->Country->save($data)) {
+                $msg['success'] = 1;
+                $msg['message'] = 'Country has been saved';
+                if ($this->request->data['Country']['id'] != '') {
+                    $msg['message'] = 'Country has been updated';
+                }
+            } else {
+                $msg['success'] = 0;
+                $msg['message'] = 'System Error, Please try again';
+            }
+        }
+        $this->set(compact('msg'));
+        $this->render("/Elements/json_messages");
+        
         
     }
     
--- app/webroot/js/Master/countries.js
+++ app/webroot/js/Master/countries.js
@@ -1,35 +1,96 @@
+var oTable;
 $(function () {
 
-    var oTable = $('#getCountry').dataTable({
+    oTable = $('#getCountry').dataTable({
         "iDisplayLength": 20,
         "bProcessing": true,
         "bServerSide": true,
         "sAjaxSource": baseUrl + "/country/getAjaxData",
         "fnCreatedRow": function (nRow, aData, iDataIndex) {
-            $('td:eq(4)', nRow).html('<a class="edit_row btn btn-xs btn-success" data-rowid=' + aData[0] + '><span class="glyphicon glyphicon-edit"></span>Edit</a> \n\
-<a class="delete_row btn btn-xs btn-danger" data-rowid=' + aData[0] + '><span class="glyphicon glyphicon-trash">Delete</a>');
+            $('td:eq(4)', nRow).html('<a class="edit_row btn btn-xs btn-success" onclick="editCountry(' + aData[0] + ', \'' + aData[1] + '\')" data-rowid=' + aData[0] + '><span class="glyphicon glyphicon-edit"></span>Edit</a> \n\
+<a class="delete_row btn btn-xs btn-danger" onclick="deleteBloodGroup(' + aData[0] + ')" data-rowid=' + aData[0] + '><span class="glyphicon glyphicon-trash">Delete</a>');
 
         },
         "rowCallback": function (row, data) {
 
         },
         "fnInitComplete": function (oSettings, json) {
-            $('.edit_row').click(function () {
-                console.log($(this).data('rowid'));
-            });
-
-            $('.delete_row').click(function () {
-                $.ajax({
-                    url: baseUrl + '/country/delete',
-                    dataType: 'json',
-                    data: {id: $(this).data('rowid')},
-                    type: "POST",
-                    success: function (response) {
-                        oTable.fnDraw(true);
-                    }
-                });
-            });
+           
         }
     });
     $('#getCountry').removeClass('display').addClass('table table-striped table-bordered');
 });
+$("#addCountry").validate({
+    errorElement: "span",
+    rules: {
+        'data[Country][name]': {
+            required: true,
+            maxlength: 25
+        }
+    },
+    messages: {
+        'data[Country][name]': {
+            required: 'Please enter country',
+            maxlength: 'Length exceeds 25 charaters'
+        }
+    },
+    submitHandler: function (form) {
+        var queryString = $('#addCountry').serialize();
+
+        $.post(baseUrl + '/country/add', queryString, function (data) {
+            if (0 == data.status) {
+                if (data.error.name.length > 0) {
+                    for (var i = 0; i < data.error.name.length; i++) {
+                        displayErrors(data.error.name[i], $("#" + data.error.name[i]).attr('type'), data.error.errormsg[i], "server");
+                    }
+                }
+            } else {
+                var displayMsg = data.message;
+                showJsSuccessMessage(displayMsg);
+                $('.addCountryForm').toggle('slow');
+                setTimeout(function () {
+                    $('.jssuccessMessage').hide('slow');
+                    oTable.fnDraw(true);
+                }, 2500);
+            }
+        }, "json");
+        return false;
+    }
+});
+$(".bgButton").click(function () {
+    $("#addCountry").submit();
+    return false;
+});
+
+$('.addcountry').click(function () {
+    $('.countryid').val('');
+    $('.addCountryForm').toggle('slow');
+});
+
+function deleteBloodGroup(id)
+{
+
+    $.ajax({
+        url: baseUrl + '/country/delete',
+        dataType: 'json',
+        data: {id: id},
+        type: "POST",
+        success: function (response) {
+            var displayMsg = response.message;
+            showJsSuccessMessage(displayMsg);
+            setTimeout(function () {
+                $('.jssuccessMessage').hide('slow');
+                oTable.fnDraw(true);
+            }, 2500);
+
+
+        }
+    });
+}
+
+function editCountry(id, name)
+{
+    $.trim($('.bname').val(name));
+    $('.countryid').val(id);
+    $('.addCountryForm').show();
+}
