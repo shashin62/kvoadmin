c907510998c7a04fc78cb0a3b52fead313c09f1b
sanily2j <shet.sanil@gmail.com>
2014-12-16 10:02:07 +0530

    Add missing data

--- app/Model/People.php
+++ app/Model/People.php
@@ -101,7 +101,7 @@ Class People extends AppModel
                 } else {
                     $sWhere .= ' AND ';
                 }
-                $sWhere .= ' p.is_late = 0 AND p.first_name is not null';
+                $sWhere .= ' p.first_name is not null';
                     break;
                 default:
                     if ($sWhere == "") {
@@ -220,7 +220,7 @@ Class People extends AppModel
     }
 
 
-    public function getPeopleData( $userId , $type = false) {
+    public function getPeopleData( $userId , $type = false , $groupId = false) {
         $this->recursive = -1;
         if( $type) {
             $options['conditions']['People.id'] = $userId;
@@ -228,7 +228,19 @@ Class People extends AppModel
             $options['conditions']['People.user_id'] = $userId;
         }
         
-          $options['fields'] = array('People.*');
+        $options['conditions']['Group.group_id'] = $groupId;
+        
+         $options['joins'] = array(
+            array('table' => 'people_groups',
+                'alias' => 'Group',
+                'type' => 'INNER',
+                'conditions' => array(
+                    'People.id = Group.people_id'
+                )
+            )
+             );
+        
+          $options['fields'] = array('People.*','Group.tree_level');
         try {
             $userData = $this->find('all', $options);
 
@@ -549,6 +561,134 @@ Class People extends AppModel
             CakeLog::write('db', __FUNCTION__ . " in " . __CLASS__ . " at " . __LINE__ . $e->getMessage());
             return false;
         }
+    }
+    
+    public function getCallAgainMembers()
+    {
+        $aColumns = array('id', 'first_name', 'last_name','mobile_number');
+
+        /* Indexed column (used for fast and accurate table cardinality) */
+        $sIndexColumn = "id";
+
+        /* DB table to use */
+        $sTable = "people";
+
+        /*
+         * Paging
+         */
+        $sLimit = "";
+        if (isset($_GET['iDisplayStart']) && $_GET['iDisplayLength'] != '-1') {
+            $sLimit = "LIMIT " . intval($_GET['iDisplayStart']) . ", " .
+                    intval($_GET['iDisplayLength']);
+        }
+
+
+        /*
+         * Ordering
+         */
+        $sOrder = "";
+        if (isset($_GET['iSortCol_0'])) {
+            $sOrder = "ORDER BY  ";
+            for ($i = 0; $i < intval($_GET['iSortingCols']); $i++) {
+                if ($_GET['bSortable_' . intval($_GET['iSortCol_' . $i])] == "true") {
+                    $sOrder .= "`" . $aColumns[intval($_GET['iSortCol_' . $i])] . "` " .
+                            ($_GET['sSortDir_' . $i] === 'asc' ? 'asc' : 'desc') . ", ";
+                }
+            }
+
+            $sOrder = substr_replace($sOrder, "", -2);
+            if ($sOrder == "ORDER BY") {
+                $sOrder = "";
+            }
+        }
+        /*
+         * Filtering
+         * NOTE this does not match the built-in DataTables filtering which does it
+         * word by word on any field. It's possible to do here, but concerned about efficiency
+         * on very large tables, and MySQL's regex functionality is very limited
+         */
+        $sWhere = "";
+        if (isset($_GET['sSearch']) && $_GET['sSearch'] != "") {
+            $sWhere = "WHERE (";
+            for ($i = 0; $i < count($aColumns); $i++) {
+                $sWhere .= "`" . $aColumns[$i] . "` LIKE '%" . ($_GET['sSearch']) . "%' OR ";
+            }
+            $sWhere = substr_replace($sWhere, "", -3);
+            $sWhere .= ')';
+        }
+        /* Individual column filtering */
+        for ($i = 0; $i < count($aColumns); $i++) {
+            if (isset($_GET['bSearchable_' . $i]) && $_GET['bSearchable_' . $i] == "true" && $_GET['sSearch_' . $i] != '') {
+                if ($sWhere == "") {
+                    $sWhere = "WHERE ";
+                } else {
+                    $sWhere .= " AND ";
+                }
+                $sWhere .= "`" . $aColumns[$i] . "` LIKE '%" . ($_GET['sSearch_' . $i]) . "%' ";
+            }
+        }
+        
+        if ($sWhere == "") {
+            $sWhere = "WHERE call_again = 1";
+        } else {
+            $sWhere .= ' AND call_again = 1';
+        }
+
+        /*
+         * SQL queries
+         * Get data to display
+         */
+
+
+        $sQuery = "
+    SELECT SQL_CALC_FOUND_ROWS `" . str_replace(" , ", " ", implode("`, `", $aColumns)) . "`
+            FROM   $sTable
+            $sWhere
+            $sOrder
+            $sLimit
+            ";
+
+        $rResult = $this->query($sQuery);
+
+        /* Data set length after filtering */
+        $sQuery = "
+    SELECT FOUND_ROWS() as total
+";
+        $rResultFilterTotal = $this->query($sQuery);
+
+        $iFilteredTotal = $rResultFilterTotal[0][0]['total'];
+
+        /* Total data set length */
+        $sQuery = "
+    SELECT COUNT(`" . $sIndexColumn . "`) as countid
+            FROM   $sTable
+            ";
+        $rResultTotal = $this->query($sQuery);
+
+        $iTotal = $rResultTotal[0][0]['countid'];
+
+
+        /*
+         * Output
+         */
+        $output = array(
+            "sEcho" => intval($_GET['sEcho']),
+            "iTotalRecords" => $iTotal,
+            "iTotalDisplayRecords" => $iFilteredTotal,
+            "aaData" => array()
+        );
+
+        foreach ($rResult as $key => $value) {
+
+            $row = array();
+            for ($i = 0; $i < count($aColumns); $i++) {
+                /* General output */
+                $row[] = $value['people'][$aColumns[$i]];
+            }
+            //$row[] = '';
+            $output['aaData'][] = $row;
+        }
+        return $output;
     }
     
 
