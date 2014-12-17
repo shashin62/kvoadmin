ec8a91cc25c6e8adf8ddcc5744a60e4c32be9b7b
sanily2j <shet.sanil@gmail.com>
2014-12-17 16:57:36 +0530

    Transfer of family

--- app/webroot/js/Family/details.js
+++ app/webroot/js/Family/details.js
@@ -1,73 +1,107 @@
-$('.self').click(function(){
-   var $this =  $(this);
-   var id = $this.data('id');
-   var gid = $this.data('gid');
-   
-   doFormPost(baseUrl+"/family/index?type=self&id="+ id+ "&gid=" + gid,'{ "type":"self","fid":"'+ id +'","gid":"'+ gid +'"}');
-   
+$('.self').click(function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    var gid = $this.data('gid');
+
+    doFormPost(baseUrl + "/family/index?type=self&id=" + id + "&gid=" + gid, '{ "type":"self","fid":"' + id + '","gid":"' + gid + '"}');
+
 });
 
-$('.editaddress').click(function(){
-   var $this =  $(this);
-   var id = $this.data('id');
-  var aid = $this.data('aid');
-  var gid = $this.data('gid');
-  
-   doFormPost(baseUrl+"/family/addAddress?type=self&id="+ id + "&aid="+ aid + "&gid=" + gid,
-   '{ "type":"self","fid":"'+ id +'","aid":"'+ aid +'","gid":"'+ gid +'"}');
-   
+$('.editaddress').click(function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    var aid = $this.data('aid');
+    var gid = $this.data('gid');
+
+    doFormPost(baseUrl + "/family/addAddress?type=self&id=" + id + "&aid=" + aid + "&gid=" + gid,
+            '{ "type":"self","fid":"' + id + '","aid":"' + aid + '","gid":"' + gid + '"}');
+
 });
 
-$('.editbusiness').click(function(){
-   var $this =  $(this);
-   var id = $this.data('id');
-  var aid = $this.data('aid');
-  var gid = $this.data('gid');
-  
-   doFormPost(baseUrl+"/family/addBusiness?type=self&id="+ id + "&aid="+ aid + "&gid=" + gid,
-   '{ "type":"self","fid":"'+ id +'","aid":"'+ aid +'","gid":"'+ gid +'"}');
-   
+$('.editbusiness').click(function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    var aid = $this.data('aid');
+    var gid = $this.data('gid');
+
+    doFormPost(baseUrl + "/family/addBusiness?type=self&id=" + id + "&aid=" + aid + "&gid=" + gid,
+            '{ "type":"self","fid":"' + id + '","aid":"' + aid + '","gid":"' + gid + '"}');
+
 });
 
 
-$('.addspouse').click(function(){
-    var $this =  $(this);
+$('.addspouse').click(function () {
+    var $this = $(this);
     var id = $this.data('id');
     var first_name = $this.data('first_name');
-     var gid = $this.data('gid');
-   doFormPost(baseUrl+"/family/index?type=addspouse",
-   '{ "type":"addspouse","fid":"'+ id +'","name_parent":"'+ first_name +'","gid":"'+ gid +'"}');
-    
+    var gid = $this.data('gid');
+    doFormPost(baseUrl + "/family/index?type=addspouse",
+            '{ "type":"addspouse","fid":"' + id + '","name_parent":"' + first_name + '","gid":"' + gid + '"}');
+
 });
 
-$('.addfather').click(function(){
-   var $this =  $(this);
-   var id = $this.data('id');
-   var gid = $this.data('gid');
-   var first_name = $this.data('first_name');
-   
-   doFormPost(baseUrl+"/family/searchPeople?type=addfather",
-   '{ "type":"addfather","fid":"'+ id +'","gid":"'+ gid +'","name_parent":"'+ first_name +'"}');
+$('.addfather').click(function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    var gid = $this.data('gid');
+    var first_name = $this.data('first_name');
+
+    doFormPost(baseUrl + "/family/searchPeople?type=addfather",
+            '{ "type":"addfather","fid":"' + id + '","gid":"' + gid + '","name_parent":"' + first_name + '"}');
 });
 
-$('.addmother').click(function(){
-   var $this =  $(this);
-   var id = $this.data('id');  
-   var gid = $this.data('gid');
-   var first_name = $this.data('first_name');
-   doFormPost(baseUrl+"/family/searchPeople?type=addmother",
-   '{ "type":"addmother","fid":"'+ id +'","gid":"'+ gid +'","name_parent":"'+ first_name +'"}');
-    
+$('.addmother').click(function () {
+    var $this = $(this);
+    var id = $this.data('id');
+    var gid = $this.data('gid');
+    var first_name = $this.data('first_name');
+    doFormPost(baseUrl + "/family/searchPeople?type=addmother",
+            '{ "type":"addmother","fid":"' + id + '","gid":"' + gid + '","name_parent":"' + first_name + '"}');
+
 });
-$('.addchild').click(function(){
+$('.addchild').click(function () {
+
+    var $this = $(this);
+    var id = $this.data('id');
+    var first_name = $this.data('first_name');
+    var gid = $this.data('gid');
+
+    doFormPost(baseUrl + "/family/index?type=addchilld",
+            '{ "type":"addchilld","fid":"' + id + '","name_parent":"' + first_name + '","gid":"' + gid + '"}');
+
+
+});
+var dialog;
+$(document).ready(function () {
+    dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 'auto',
+        width: 'auto',
+        modal: true,
+        buttons: {
+           
+                "Submit": transferUser,
+                Cancel: function () {
+                    dialog.dialog("close");
+                
+            }
+        },
+        close: function () {
+
+
+        }
+    });
+
+});
+
+function transferUser()
+{
     
-   var $this =  $(this);
-   var id = $this.data('id');
-  var first_name = $this.data('first_name');
-  var gid = $this.data('gid');
- 
-   doFormPost(baseUrl+"/family/index?type=addchilld",
-   '{ "type":"addchilld","fid":"'+ id +'","name_parent":"'+ first_name +'","gid":"'+ gid +'"}');
-   
     
+}
+
+$(".transfer-family").on("click", function () {
+    
+    $("#dialog-form").data('id',$(this).data('id')).dialog("open");
+    return false;
 });
