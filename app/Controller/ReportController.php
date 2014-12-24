76566098bf3935b05b206b74c449ea1433feec48
sanily2j <shet.sanil@gmail.com>
2014-12-24 12:08:36 +0530

    Add translations

---
+++ app/Controller/ReportController.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Controller file
+ *
+ * PHP version 5.5
+ *
+ * @category  Controller
+ * @package   KVO Admin
+ * @author    S
+ * @copyright 2014 KVO Admin
+ * @version   GIT:$
+ * @since     1.0
+ */
+App::uses('AppController', 'Controller');
+
+/**
+ * Controller class for Report Controller
+ * 
+ * @category  Controiller
+ * @package   KVO Admin
+ * @author    S
+ * @copyright 2014 KVO Admin
+ * @since     1.0
+ */
+Class ReportController extends AppController {
+    
+     public $name = 'Report';
+    public $uses = array('User','Translation');
+    public $helpers = array('Session');
+    public $components = array('Session');
+    
+    public function reports()
+    {
+        
+    }
+    
+    public function getReportsData()
+    {
+        $this->autoRender = false;
+        $data = $this->Translation->getAllTranslations(true);
+        echo json_encode($data);
+        
+    }
+}
