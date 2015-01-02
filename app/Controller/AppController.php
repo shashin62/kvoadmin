a8b557145596daee18a8b73ca7237963201ad273
sanily2j <shet.sanil@gmail.com>
2015-01-02 11:12:26 +0530

    Note Feature

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -49,7 +49,7 @@ class AppController extends Controller {
             'loginRedirect' => array('controller' => 'user', 'action' => 'welcome'),
             'authError' => 'You are not authorized to view this page',
             'allowedActions' => array('logout','register','doRegisterUser','buildTreeJson',
-                'rebuildARO','callAgain'),
+                'rebuildARO','callAgain','addNote','viewNote'),
 //            'authenticate' => array(
 //                'all' => array('userModel' => 'User'),
 //                'Form' => array(),
