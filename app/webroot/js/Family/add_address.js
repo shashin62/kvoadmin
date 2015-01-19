7a36a42643f1c12f6c98965678f869f346dd8ed2
Sanil Shet <shet.sanil@gmail.com>
2015-01-19 21:32:23 +0530

    links

--- app/webroot/js/Family/add_address.js
+++ app/webroot/js/Family/add_address.js
@@ -66,7 +66,8 @@ $( ".combobox" ).combobox({width: '200px'});
             var queryString = $('#addressForm').serialize();
             var peopleid = pid;
             var addressid = aid;
-            
+             queryString += '&data[Address][suburb]='+ $('.suburbdiv').find('.ui-autocomplete-input').val();
+            queryString += '&data[Address][state]='+ $('.statesdiv').find('.ui-autocomplete-input').val();
             $.post(baseUrl + '/family/doProcessAddress?peopleid=' + peopleid + '&addressid=' + addressid + '&parentid=' + prntid, queryString, function (data) {
                  if (0 == data.status) {
                 if (data.error.name.length > 0) {
