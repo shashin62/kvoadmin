669f21f60d4078172830bf317dafb8cec4adb229
Sanil Shet <shet.sanil@gmail.com>
2015-01-29 21:55:31 +0530

    people report

--- app/webroot/js/Reports/missing_data.js
+++ app/webroot/js/Reports/missing_data.js
@@ -7,7 +7,7 @@ $(function () {
         "bServerSide": true,
         "sAjaxSource": baseUrl + "/report/getMissingRecords",
         "fnCreatedRow": function (nRow, aData, iDataIndex) {
-            $('td:eq(4)', nRow).html('<a class="edit_row btn btn-xs btn-success" onclick=""><span class="glyphicon glyphicon-edit"></span>Edit</a>');
+            $('td:eq(5)', nRow).html('<a onclick="editMissingRecords(' + aData[0] + ', \'' + aData[1] + '\')" data-rowid=' + aData[0] + ' class="edit_row btn btn-xs btn-success" onclick=""><span class="glyphicon glyphicon-edit"></span>Edit</a>');
         },
         "rowCallback": function (row, data) {
 
@@ -18,3 +18,8 @@ $(function () {
     });
     $('#getMissingData').removeClass('display').addClass('table table-striped table-bordered');
 });
+
+function editMissingRecords(id, groupid)
+{
+    window.location.href = baseUrl + '/family/details/' + groupid;
+}
