a792f0fefe0b1dace62269108a1985e6653a5154
Sanil Shet <shet.sanil@gmail.com>
2015-01-31 12:44:47 +0530

    activate/deactivate user

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -52,7 +52,7 @@ class AppController extends Controller {
                 'rebuildARO','callAgain','addNote','viewNote','getAutoCompleteFirstName',
                 'getAutoCompleteLastName','populateZipCodeData','getZipCodesData',
                 'getNatureBusinessData','getTypeBusinessData','getSpecialBusinessData','getZipAjaxData'
-                ,'index','addZip','deleteZip','getMissingRecords','records'),
+                ,'index','addZip','deleteZip','getMissingRecords','records','changestatus'),
 //            'authenticate' => array(
 //                'all' => array('userModel' => 'User'),
 //                'Form' => array(),
