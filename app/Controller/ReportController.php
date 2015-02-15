69a75c7838c15c58aeb1d20f7ae1b5d0e9bf003d
sanil <sanil@sanil-PC>
2015-02-15 14:16:20 -0800

    all reports

--- app/Controller/ReportController.php
+++ app/Controller/ReportController.php
@@ -26,7 +26,7 @@ App::uses('AppController', 'Controller');
 Class ReportController extends AppController {
 
     public $name = 'Report';
-    public $uses = array('User', 'Translation', 'People');
+    public $uses = array('User', 'Translation', 'People','Village');
     public $helpers = array('Session');
     public $components = array('Session');
 
@@ -43,22 +43,22 @@ Class ReportController extends AppController {
 
     public function records() {
         $roleID = $this->Session->read('User.role_id');
-        $this->set('roleID',$roleID);
-       $operators =   $this->User->getOperatorsList();
-         $this->set(compact('operators'));
+        $this->set('roleID', $roleID);
+        $operators = $this->User->getOperatorsList();
+        $this->set(compact('operators'));
     }
-    
+
     public function completedrecords() {
         
     }
-    
-     public function getCompletedRecords() {
+
+    public function getCompletedRecords() {
         $userID = $this->Session->read('User.user_id');
         $roleID = $this->Session->read('User.role_id');
         $_REQUEST['fromdate'] = $_REQUEST['fromdate'] ? $_REQUEST['fromdate'] : '';
         $_REQUEST['todate'] = $_REQUEST['todate'] ? $_REQUEST['todate'] : '';
         $this->autoRender = false;
-        $data = $this->People->getCompletedRecords($userID, $roleID, $_REQUEST['fromdate'],$_REQUEST['todate'] );
+        $data = $this->People->getCompletedRecords($userID, $roleID, $_REQUEST['fromdate'], $_REQUEST['todate']);
         echo json_encode($data);
     }
 
@@ -66,10 +66,47 @@ Class ReportController extends AppController {
         $userID = $this->Session->read('User.user_id');
         $roleID = $this->Session->read('User.role_id');
         $this->autoRender = false;
-         $_REQUEST['userid'] = $_REQUEST['userid'] ? $_REQUEST['userid'] : '';
+        $_REQUEST['userid'] = $_REQUEST['userid'] ? $_REQUEST['userid'] : '';
         $data = $this->People->getMissingData($userID, $roleID, $_REQUEST['userid']);
         echo json_encode($data);
     }
-    
-  
+
+    public function all() {
+        $nature_of_business = $this->People->fetchNatureBusniessName();
+        $this->set(compact('nature_of_business'));
+        
+        $specialty_business_service = $this->People->fetchSpecialityBusniessName();
+        $this->set(compact('specialty_business_service'));
+        
+        $businesstypename = $this->People->fetchBusniessTypeName();
+        $this->set(compact('businesstypename'));
+        
+        $businessname = $this->People->fetchBusniessName();
+        $this->set(compact('businessname'));
+        
+         $villages = $this->Village->find('list', array('fields' => array('Village.name', 'Village.name')));
+        $this->set(compact('villages'));
+        
+         $occupation = $this->People-> fetchOccupation();
+        $this->set(compact('occupation'));
+        
+         $date_of_birth = $this->People->fetchDateofBirth();
+         $dobs = array();
+          foreach ($date_of_birth as $key => $value) {
+              $dobs[$value[0]['date_of_birth']] = $value[0]['date_of_birth'];
+          }
+        // echo '<pre>';print_r($date_of_birth);exit;
+        $this->set(compact('dobs'));
+       
+    }
+
+    public function getAllAjaxData() {
+       $userID = $this->Session->read('User.user_id');
+        $roleID = $this->Session->read('User.role_id');
+        $this->autoRender = false;
+       // echo '<pre>';print_r($_REQUEST);exit;
+        $_REQUEST['userid'] = $_REQUEST['userid'] ? $_REQUEST['userid'] : '';
+        $data = $this->People->getAllData($userID, $roleID, $_REQUEST);
+        echo json_encode($data);
+    }
 }
