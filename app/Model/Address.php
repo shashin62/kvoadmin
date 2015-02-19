50b2e8899ea4dd718f9cbbcf155f7c37bbce51aa
Sanil Shet <shet.sanil@gmail.com>
2015-02-18 22:49:17 -0800

    Busniess Address

--- app/Model/Address.php
+++ app/Model/Address.php
@@ -2,6 +2,69 @@
 Class Address extends AppModel {
     var $name = "Address";
     public $useTable = 'address';
+    
+    public function getCity()
+    {
+        $this->recursive = -1;
+        $options['conditions'] = array(
+                            'Address.city is not null'
+                            );
+        $options['fields'] = array('Address.city','Address.city');
+        $options['group'] = array('Address.city');
+        try {
+            $userData = $this->find('list', $options);
+            if ($userData) {
+                return $userData;
+            } else {
+                return array();
+            }
+        } catch (Exception $e) {
+            CakeLog::write('db', __FUNCTION__ . " in " . __CLASS__ . " at " . __LINE__ . $e->getMessage());
+            return false;
+        }
+    }
+    
+     public function getSuburb()
+    {
+        $this->recursive = -1;
+        $options['conditions'] = array(
+                            'Address.suburb is not null'
+                            );
+        $options['fields'] = array('Address.suburb','Address.suburb');
+        $options['group'] = array('Address.suburb');
+        try {
+            $userData = $this->find('list', $options);
+            if ($userData) {
+                return $userData;
+            } else {
+                return array();
+            }
+        } catch (Exception $e) {
+            CakeLog::write('db', __FUNCTION__ . " in " . __CLASS__ . " at " . __LINE__ . $e->getMessage());
+            return false;
+        }
+    }
+    
+    public function getStates()
+    {
+        $this->recursive = -1;
+        $options['conditions'] = array(
+                            'Address.state is not null'
+                            );
+        $options['fields'] = array('Address.state','Address.state');
+        $options['group'] = array('Address.state');
+        try {
+            $userData = $this->find('list', $options);
+            if ($userData) {
+                return $userData;
+            } else {
+                return array();
+            }
+        } catch (Exception $e) {
+            CakeLog::write('db', __FUNCTION__ . " in " . __CLASS__ . " at " . __LINE__ . $e->getMessage());
+            return false;
+        }
+    }
 }
 /* 
  * To change this license header, choose License Headers in Project Properties.
