87ba43c41547c4ef8e28ff7d1f732356da4c31cb
Swatirani <swatirani.senapati@in.v2solutions.com>
2015-04-21 18:46:01 +0530

    tree nav fixes

--- app/Controller/FamilyController.php
+++ app/Controller/FamilyController.php
@@ -1227,6 +1227,7 @@ Class FamilyController extends AppController {
 
         $jsonData['tree'] = $tree;
         $jsonData['parent_name'] = $peopleRootData['first_name'] . ' ' . $peopleRootData['last_name'];
+        $jsonData['count'] = count($tree);
 
         echo json_encode($jsonData);
         exit;
--- app/webroot/tree/index.php
+++ app/webroot/tree/index.php
@@ -6,7 +6,7 @@ if (isset($_GET['full'])) {
 	$json_data = file_get_contents('http://kvo.quadzero.in/people/index/export_as_json:1/group_id:' . $_GET['group_id']);
 } else {
 	//$json_data = file_get_contents('http://10.50.249.127/kvoadmin/family/buildTreeJson?gid=' . $_GET['gid'] . ' &uid=1');
-        $json_data = file_get_contents('http://admin.kvomahajan.com/family/buildTreeJson?gid=' . $_GET["gid"]);
+        $json_data = file_get_contents('http://localhost/kvoadmin/family/buildTreeJson?gid=' . $_GET["gid"]);
 }
 
 ?>
@@ -380,7 +380,7 @@ if (isset($_GET['full'])) {
             navshowchildren = window.navframe.document.getElementById('showchildren').value;
             navshowcousins = window.navframe.document.getElementById('showcousins').value;
 
-            new Ajax.Request('http://admin.kvomahajan.com/family/buildFamilyJson?id='+id, {
+            new Ajax.Request('http://localhost/kvoadmin/family/buildFamilyJson?id='+id, {
                 method: 'get',
                 onComplete:function(_2d){
                     data = _2d.responseText;
@@ -410,6 +410,7 @@ if (isset($_GET['full'])) {
                         sideframeContent.PL(); 
                     }
                     document.getElementById('lfamilyname').innerHTML = 'Family of '  + parsedData['parent_name'];
+                    window.navframe.NCP(parseInt(parsedData['count']));
                 }
             });
         }
