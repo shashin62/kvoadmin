6ca305d448dd4378853e27a35aefa7fdfc40367c
Swatirani <swatirani.senapati@in.v2solutions.com>
2015-05-06 16:20:30 +0530

    changes on people education

--- app/Controller/FamilyController.php
+++ app/Controller/FamilyController.php
@@ -2049,14 +2049,14 @@ Class FamilyController extends AppController {
         $pdata = array();
         foreach ($data as $key => $val) {
             $fdata = array();
-            $fdata[] = $val['people_educations']['id'];
             $fdata[] = ($val['people_educations']['name']) ? $val['people_educations']['name'] : '';
             $fdata[] = ($val['people_educations']['institution_name']) ? $val['people_educations']['institution_name'] : '';
             $fdata[] = ($val['people_educations']['university_name']) ? $val['people_educations']['university_name'] : '';
             $fdata[] = ($val['people_educations']['area_specialization']) ? $val['people_educations']['area_specialization'] : '';
             $fdata[] = ($val['people_educations']['year_of_passing']) ? $val['people_educations']['year_of_passing'] : '';
             $fdata[] = ($val['people_educations']['percentage']) ? $val['people_educations']['percentage'] : '';
-            $fdata[] = ($val['people_educations']['part_full_time']) ? $val['people_educations']['part_full_time'] : '';
+            $fdata[] = ($val['people_educations']['part_full_time']) ? $val['people_educations']['part_full_time'] : '';            
+            $fdata[] = $val['people_educations']['id'];
             $fdata[] = $val['people_educations']['people_id'];
             
             $pdata[] = $fdata;
